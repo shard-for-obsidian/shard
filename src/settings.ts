51abb5decb69b2a6311d5c2a6307d13fcb5d4579
Andrew Gillis <gillis.andrew@gmail.com>
2026-02-03 14:03:07 -0500

    wip

--- src/settings.ts
+++ src/settings.ts
@@ -1,35 +1,30 @@
-import { App, PluginSettingTab, Setting } from 'obsidian';
-import type GHCRTagBrowserPlugin from './main';
+import { App, PluginSettingTab, Setting } from "obsidian";
+import type GHCRTagBrowserPlugin from "./main";
 
 export class GHCRSettingTab extends PluginSettingTab {
-	plugin: GHCRTagBrowserPlugin;
+  plugin: GHCRTagBrowserPlugin;
 
-	constructor(app: App, plugin: GHCRTagBrowserPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: GHCRTagBrowserPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const { containerEl } = this;
-		containerEl.empty();
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
 
-		containerEl.createEl('h2', { text: 'GHCR Tag Browser Settings' });
-
-		new Setting(containerEl)
-			.setName('GitHub Token')
-			.setDesc('Optional personal access token for private repositories')
-			.addText(text => text
-				.setPlaceholder('ghp_...')
-				.setValue(this.plugin.settings.githubToken)
-				.onChange(async (value) => {
-					this.plugin.settings.githubToken = value;
-					await this.plugin.saveSettings();
-				}));
-
-		// Make the input a password field
-		const tokenInput = containerEl.querySelector('input[type="text"]');
-		if (tokenInput) {
-			tokenInput.setAttribute('type', 'password');
-		}
-	}
+    new Setting(containerEl)
+      .setName("GitHub Token")
+      .setDesc("Optional personal access token for private repositories")
+      .addText((text) =>
+        text
+          .setPlaceholder("ghp_...")
+          .setValue(this.plugin.settings.githubToken)
+          .onChange(async (value) => {
+            this.plugin.settings.githubToken = value;
+            await this.plugin.saveSettings();
+          })
+          .inputEl.setAttribute("type", "password"),
+      );
+  }
 }
