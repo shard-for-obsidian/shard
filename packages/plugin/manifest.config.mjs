19a7d8018dad2162631d36606dc716c29e64bece
Andrew Gillis <gillis.andrew@gmail.com>
2026-02-05 08:51:22 -0500

    wip: license and branding

--- packages/plugin/manifest.config.mjs
+++ packages/plugin/manifest.config.mjs
@@ -1,9 +1,13 @@
 import packageJson from "./package.json" assert { type: "json" };
 
+// Extract Obsidian version from dependencies (removing ^ if present)
+const obsidianVersion = (packageJson.dependencies?.obsidian || "").replace(/^\^/, "");
+
 export const manifest = {
   id: packageJson.name,
   name: packageJson.name,
   version: packageJson.version,
   description: packageJson.description,
-  author: packageJson.author
-}
+  author: packageJson.author,
+  minAppVersion: obsidianVersion,
+};
