8466ed286bb5abd846617b0906b5f94df6630a36
Andrew Gillis <gillis.andrew@gmail.com>
2026-02-10 08:49:21 -0500

    feat(lib): add Zod transform for OCI annotations to frontmatter
    Provides type-safe transformation from GHCR OCI manifest annotations
    to marketplace markdown frontmatter structure.

--- packages/lib/src/marketplace/index.ts
+++ packages/lib/src/marketplace/index.ts
@@ -3,3 +3,5 @@ export {
   DEFAULT_MARKETPLACE_URL,
   type MarketplaceClientOptions,
 } from "./client.js";
+
+export { ociAnnotationsToFrontmatter, type PluginFrontmatter } from './oci-to-markdown.js';
